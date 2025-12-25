995a6ee8c5bc2ca10c8d28089eca0c34bb45d0ba
front2-galaxy-it <94291571+Boskolife@users.noreply.github.com>
2025-12-25 19:09:45 +0200

    feat: Add logo settings for multiple languages and enhance hero section autoplay behavior

--- assets/hero-section.js
+++ assets/hero-section.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         autoplay: {
             delay: 0,
             disableOnInteraction: false,
+            pauseOnMouseEnter: true,
         },
 
         freeMode: {
